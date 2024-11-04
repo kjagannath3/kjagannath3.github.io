215cac500efab3daf6993f6d7a1620f4d04f8818
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-11-04 16:48:55 -0500

    Will try to implement the accordion from scratch, final deploy check

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -39,13 +39,68 @@
             </div>
         </content>
     </imagetitle>
-    <internshipList class="grid place-items-center justify-center">
+    <internshipList>
         <h1 class="text-5xl font-bold text-rose-400">
             Take a look at some of the companies I have applied to!
         </h1>
         <Accordion>
-            <AccordionItem></AccordionItem>
+            <AccordionItem>
+                <span slot="header">My Header 1</span>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Illo ab necessitatibus sint explicabo ...
+                </p>
+                <p class="text-gray-500 dark:text-gray-400">
+                    Check out this guide to learn how to <a
+                        href="/"
+                        target="_blank"
+                        rel="noreferrer"
+                        class="text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                        get started
+                    </a>
+                    and start developing websites even faster with components on
+                    top of Tailwind CSS.
+                </p>
+            </AccordionItem>
+            <AccordionItem>
+                <span slot="header">My Header 2</span>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Illo ab necessitatibus sint explicabo ...
+                </p>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Illo ab necessitatibus sint explicabo ...
+                </p>
+                <p class="mb-2 text-gray-500 dark:text-gray-400">
+                    Learn more about these technologies:
+                </p>
+                <ul class="list-disc ps-5 dark:text-gray-400 text-gray-500">
+                    <li>
+                        <a
+                            href="/"
+                            target="_blank"
+                            rel="noreferrer"
+                            class="text-blue-600 dark:text-blue-500 hover:underline"
+                        >
+                            Lorem ipsum
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://tailwindui.com/"
+                            rel="noreferrer"
+                            target="_blank"
+                            class="text-blue-600 dark:text-blue-500 hover:underline"
+                        >
+                            Tailwind UI
+                        </a>
+                    </li>
+                </ul>
+            </AccordionItem>
         </Accordion>
+    </internshipList>
     <callToAction class="mx-auto mt-64 grid place-items-center">
         <h3 class="text-5xl font-bold text-rose-400">Let's get in touch!</h3>
         <a
